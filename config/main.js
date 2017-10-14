8d5832bf4a87d448485c8201c52fd8dc450d9a1f
pliik <ceo@pliik.com>
2017-10-14 20:59:23 +0100

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,18 +1,19 @@
 var Config = {
   market        : 'Gugamarket',
   domain        : 'gugamarket.com',
-  url           : process.env.BASE_URL || "'http://127.0.0.1:8080",
+  url           : process.env.BASE_URL || "http://127.0.0.1:8080",
   desc          : 'NodeJS REST API',
   crypto        : process.env.CRYPTO || 'TheBiGsecret',
   basecontact   : "ceo@gugamarket.com",
-  baseurl       : process.env.BASE_URL || "'http://127.0.0.1:8080",
+  baseurl       : process.env.BASE_URL || "http://127.0.0.1:8080",
   ip            : process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   port          : process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   dbPath        : process.env.MONGO_URL,
-  // To use gmail please activate less secure apps
-  // https://www.google.com/settings/security/lesssecureapps
-  yourPwd       : process.env.MAIL_PASS || 'mypassword',
-  yourSmtp      : process.env.MAIL_HOST || 'smtp.gmail.com',
+  
+  // To use gmail please activate less secure apps - https://www.google.com/settings/security/lesssecureapps
+  // Gmail less secure require yourSSL set to true.
+  yourPwd       : process.env.MAIL_PASS  || 'mypassword',
+  yourSmtp      : process.env.MAIL_HOST  || 'smtp.gmail.com',
   yourEmail     : process.env.MAIL_EMAIL || 'myaccount@gmail.com',
   yourSSL       : false
 }
